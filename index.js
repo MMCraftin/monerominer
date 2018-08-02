4471442ff2fee1594b4dec4ab39b2a35dc3bdb75
MMCraftin <MMCraftin@users.noreply.github.com>
2018-08-02 12:10:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('OwZyZ3HVKjMeMTrodeXC2iZ7ZGY8eOdT'); // Coin-Hive's Site Key
+  const miner = await CoinHive('Ih83P7rmimXTm5LPlXR1V2RBjaIvqTjZ'); // Coin-Hive's Site Key
  
   // Start miner
   await miner.start();
